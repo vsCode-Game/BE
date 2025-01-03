35a5b3aa988d3c6d1cef259ee282ccf0e34d184e
friend-0228 <kwj2712@gamil.com>
2025-01-03 18:53:51 +0900

    Chore: main.ts에서 HTTP 서버를 명시적으로 생성

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import { IoAdapter } from '@nestjs/platform-socket.io'; // IoAdapter 추가
+import * as http from 'http'; // HTTP 모듈 추가
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +16,7 @@ async function bootstrap() {
   // console.log('Routes:', server.getHttpServer()._events.request._router.stack);
 
   // WebSocket과 HTTP 서버를 통합
-  const httpServer = app.getHttpAdapter().getInstance(); // HTTP 서버 인스턴스
+  const httpServer = http.createServer(app.getHttpAdapter().getInstance()); // HTTP 서버 인스턴스
   app.useWebSocketAdapter(new IoAdapter(httpServer)); // WebSocket과 HTTP 통합
 
   await app.listen(process.env.PORT ?? 3000);
