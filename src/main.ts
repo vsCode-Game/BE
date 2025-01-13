8c1bcb55636233bf9bcea1899c96acdf8b40d188
윤준수 <99115509+hoheesu@users.noreply.github.com>
2025-01-14 02:10:16 +0900

    fix: 코스에러 수정

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'https://api.davincicodegame.store',
+    ],
     credentials: true,
   });
   app.use(cookieParser());
