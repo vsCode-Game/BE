af04afd999962e3b768a3d26a175511c296c855b
윤준수 <99115509+hoheesu@users.noreply.github.com>
2025-02-17 20:45:11 +0900

    fix: CORS 에러발생으로 인한 주소 추가

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
       'http://localhost:5173',
       'http://localhost:3000',
       'https://davincicodegame.store',
+      'https://www.davincicodegame.store',
     ],
     credentials: true,
   });
